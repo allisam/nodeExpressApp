144d0b4326f74c0bc87d18f825f5b2d5743258ef
Allison R Samson <allisamson@gmail.com>
2016-07-12 15:25:49 -0400

    views practice

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 var express = require('express');
 
-var app = express.createServer();
+var app = express();
 
-app.get('/about', function(){
-	res.send('<h1>About Us</h1>');
+app.get('/', function(req,res){
+		res.render('index.ejs',{title: 'Hello Sadie'});
+		
 });
+
+app.get('/about', function(req,res){
+		res.render('layout.ejs',{title: 'About Sadie', body:'<h1>Sadie is the best Cat</h1>'});
+		
+});
+
+app.get('/*', function(req,res){
+		res.status(404).render('error.ejs',{title: 'Error'});
+		
+});
+
+
+console.log('App running of localHost:3000');
+app.listen(3000);
