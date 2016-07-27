ab8a357553c0e60dde5e1891291eadf412b08765
Allison R Samson <allisamson@gmail.com>
2016-07-27 10:50:56 -0400

    Finished app

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 //Module Dependencies
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
 var stylus = require('stylus');
 var nib = require('nib');
 var logger = require('morgan');
+var routes = require('./routes');
+//var user = require('./routes/user');
+var http = require('http')
+var path = require('path');
+var mongo = require('mongodb');
+var monk = require('monk')
+var db = monk('localhost:27017/nodeapp');
+
 
 //Set port number
 var portnumber = 3000;
@@ -26,6 +38,32 @@ app.set('view engine', 'jade');
 console.log('Jade has been initialized');
 
 //Stylus Middleware
+
+//Express 4 express.json
+app.use(bodyParser.json());
+
+//Express 4 express.urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//Express 4 express.methodOverride
+// override with different headers; last one takes precedence 
+app.use(methodOverride('X-HTTP-Method'))          // Microsoft 
+app.use(methodOverride('X-HTTP-Method-Override')) // Google/GData 
+app.use(methodOverride('X-Method-Override'))      // IBM 
+
+//Express 4 express.cookie-parser
+app.use(cookieParser('mykey'));
+
+//express.session is now just session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+//Express 4 app.use(app.router) no longer necessary 
+
 app.use(logger('dev'));
 app.use(stylus.middleware(
 	{
@@ -35,12 +73,17 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname+'/public'));
 
+//Add routes
+app.get('/', routes.index);
+app.get('/userlist', routes.userlist(db));
+app.post('/adduser', routes.adduser(db));
+
 //Render Index Page
-app.get('/', function(req, res){
-	res.render('index',
-	{title: 'Welcome'}
-	);	
-});
+	//app.get('/', function(req, res){
+		//res.render('index',
+		//{title: 'Welcome'}
+		//);	
+	//});
 
 app.listen(portnumber);
 console.log('Server started at ' +portnumber);
